31f67f7aca22ac3d6c94ce0be98c3625781bbb94
Xiao Yuting <xiao_yuting@qq.com>
2020-07-18 20:54:48 +0800

    添加牛顿迭代法解sqrt

---
+++ source/sqrt.cpp
@@ -0,0 +1,18 @@
+//
+// 牛顿迭代法解平方根
+//
+class Solution {
+    int s;
+public:
+    int mySqrt(int x) {
+        s = x;
+        if ( x == 0 ) return 0;
+        return (int)sqrts(x);
+    }
+
+    double sqrts(double x) {
+        double res = ( x + s / x ) / 2;
+        if ( res == x ) return x;        
+        else return sqrts(res);        
+    }
+};
